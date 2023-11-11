420cb83528b05103b7aa55690f8bb41c7400e70c
Tretiakov Yehor <egorken3v@gmial.com>
2023-11-12 01:20:35 +0200

    slow-generation: Anable generation

--- src/core/game/generator.rs
+++ src/core/game/generator.rs
@@ -16,22 +16,22 @@ pub struct DefaultGameGenerator {}
 
 impl GameGenerator for DefaultGameGenerator {
     fn generate_game() -> Game {
-        // let game_map = DefaultMapGenerator::generate_map();
+        let game_map = DefaultMapGenerator::generate_map();
         
-        // let pacman = Pacman::new(game_map.map_graph.pacman_pos, Duration::milliseconds(1), Utc::now());
+        let pacman = Pacman::new(game_map.map_graph.pacman_pos, Duration::milliseconds(1), Utc::now());
         
-        // let mut ghosts = Vec::new();
-        // for ghost_pos in &game_map.map_graph.ghosts_pos {
-        //     ghosts.push(Ghost::new(*ghost_pos, game_map.map_graph.pacman_pos, Duration::milliseconds(256), Utc::now()))
-        // }
+        let mut ghosts = Vec::new();
+        for ghost_pos in &game_map.map_graph.ghosts_pos {
+            ghosts.push(Ghost::new(*ghost_pos, game_map.map_graph.pacman_pos, Duration::milliseconds(256), Utc::now()))
+        }
 
-        // Game::new(
-        //         pacman,
-        //         ghosts,
-        //         game_map,
-        // )
+        Game::new(
+                pacman,
+                ghosts,
+                game_map,
+        )
         // Game::load_from_file("/home/tr3tiakoff/University/RustPacman/res/test_map.txt")
-        Game::load_from_file("/home/tr3tiakoff/University/RustPacman/res/default_map.txt")
+        // Game::load_from_file("/home/tr3tiakoff/University/RustPacman/res/default_map.txt")
         // Game::load_from_file("/home/tr3tiakoff/University/RustPacman/res/template.txt")
     }
 }
